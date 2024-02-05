f9628413530cd4bbdbee5557a1c127404db7c22b
Lavanya <lavanyakoppolu976@gamil.com>
2024-02-05 15:10:29 +0530

    added num.py file

---
+++ num.py
@@ -0,0 +1,38 @@
+#positive or negitive
+"""num=int(input("enter the number"))
+if num>0:
+    print(num,"is positive")
+else:
+    print(num,"is negitive")"""
+#greater than a and b
+"""a=123
+b=56
+if a<b:
+    print(a,"is greater than",b)
+else:
+    print(b,"is greater than",a)"""
+#age of the person
+"""age=int(input("enter the age of the person"))
+if age>=18:
+    print("issue driving licence")
+else:
+    print("you are not eligible for licence")"""
+#nested if statement
+"""num=int(input("enter the number"))
+if num<0:
+    print(num,"is negitive number")
+elif num>0:
+    print(num,"is positive number")
+elif num==0:
+    print(num,"is equal to zero")"""
+#greater than a,b and c
+"""a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+c=int(input("enter the value of c:"))
+if a>b and a>c:
+    print(a,"is greater than ",b,"and",c)
+elif b>a and b>c:
+    print(b,"is greater than ",a,"and",c)
+elif c>a and c>b:
+    print(c,"is greater than ",a,"and",b)"""
+        
